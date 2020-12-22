33e89193f8ffe3d4e4d173f184c37d5ba3c9e8b2
Brandon <freshdaddyb@gmail.com>
2020-12-22 00:56:15 +0000

    Checkout template initialized

---
+++ src/views/store/Checkout.vue
@@ -0,0 +1,13 @@
+<template>
+<!--Checkout is cart details overview + shipping selection and moves to order confirmed page after with email details and timeframe.-->
+</template>
+
+<script>
+export default {
+name: "Checkout"
+}
+</script>
+
+<style scoped>
+
+</style>
